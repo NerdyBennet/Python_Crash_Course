4e1e2e2fb8be11e99ab7560527032461ebec0327
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-09-06 21:12:53 +0200

    Add files via upload

---
+++ Chapter 3 Introducing Lists/every_function.py
@@ -0,0 +1,42 @@
+languages = []
+languages.append("german") # 1
+languages.insert(1, "english") # 2
+languages.append("chinese")
+languages.append("dutch")
+languages.append("mandarin")
+languages.append("french")
+languages.append("spanish")
+languages.append("hindi")
+languages.append("turkish")
+
+
+print(f"{languages[0].title()}") # 3
+print(f"{languages[1].title()}")
+
+del languages[1] # 4
+print(languages)
+
+popped_languages = languages.pop() # 5 
+print(popped_languages)
+
+languages.remove("chinese") # 6
+print(languages)
+
+languages.sort() # 7 
+print(languages)
+
+languages.sort(reverse=True) # 8
+print(languages)
+
+print(sorted(languages)) # 9
+
+print(sorted(languages, reverse=True)) # 10
+
+languages.sort()
+print(languages)
+
+languages.reverse() # 11
+print(languages)
+
+print(len(languages)) # 12
+
---
+++ Chapter 3 Introducing Lists/names.py
@@ -0,0 +1,10 @@
+friends = ["paul", "aiden", "jack", "leon"]
+print(friends[0].title())
+print(friends[1].title())
+print(friends[2].title())
+print(friends[3].title())
+
+print(f"Hey {friends[0].title()} wanna play some games?")
+print(f"Hey {friends[1].title()} wanna play some games?")
+print(f"Hey {friends[2].title()} wanna play some games?")
+print(f"Hey {friends[3].title()} wanna play some games?")
