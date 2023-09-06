eba2fad0beb9d3d4016c2b29c160ffe56dfa804c
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-09-06 21:11:57 +0200

    Delete Chapter 3 Introducing Lists directory

--- Chapter 3 Introducing Lists/list.py
+++
@@ -1,4 +0,0 @@
-the_family = ["mom", "grandpa", "grandma"]#starts with the index 0
-#print(the_family[0].upper())
-message = f"My {the_family[1].title()} is the oldest of the family."
-print(message)
