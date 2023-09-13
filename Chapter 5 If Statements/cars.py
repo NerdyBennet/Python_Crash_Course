d9d931d16393c67c47310d64079a262bf3625d99
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-09-13 22:15:13 +0200

    Add files via upload
    I just started chapter 5 :)

---
+++ Chapter 5 If Statements/cars.py
@@ -0,0 +1,7 @@
+cars = ['audi', 'bmw', 'subaro', 'toyota']
+
+for car in cars:
+    if car == 'bmw': # if car is in the list print it in uppercase
+        print(car.upper())
+    else:
+        print(car.title())
