aa4b2d44155ec33f58ab00181fc0401b9d082a52
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-10-11 23:03:02 +0200

    Delete toppings.py

--- toppings.py
+++
@@ -1,8 +0,0 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
-
-for requested_topping in requested_toppings: 
-    # requested_topping without
-    #the s
-    print(f"Adding {requested_toppings}.")
-
-print("\nFinished making your pizza!")
