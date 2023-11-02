344cd23e72db1b05addb96b2dd123d4f6c4d3946
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-11-03 00:02:33 +0100

    Delete alien_colors_#3(2).py

--- alien_colors_#3(2).py
+++
@@ -1,9 +0,0 @@
-alien_color = 'yellow'
-
-if 'green' in alien_color:
-    points = 5
-elif 'yellow' in alien_color:
-    points = 10
-else: 
-    points = 15
-print(f"You just earned {points} points!")
