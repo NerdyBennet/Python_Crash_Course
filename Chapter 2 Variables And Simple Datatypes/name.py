8f35cfca4c9243db5dad2df64e0ee05ea859e729
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-11-03 00:06:28 +0100

    Add files via upload

--- Chapter 2 Variables And Simple Datatypes/name.py
+++ Chapter 2 Variables And Simple Datatypes/name.py
@@ -1,6 +1,6 @@
 name = "olaf peter"
 print(name.title()) # title() puts the first letter of the words 
-#assosiated with it in capital
+# assosiated with it in capital
 print(name.upper())
 print(name.lower())
 
@@ -13,7 +13,7 @@ print(message)
 print("\tname") # tab output
 print("names that came to mind spontaneously" +
 ":\n\tAlfred\n\tAlberto\nRoberto\n\tLars")
-#new line(Zeilenumbruch) you can't do \t\n bc it wont make sense
+# new line(Zeilenumbruch) you can't do \t\n bc it wont make sense
 
 name_with_spaces = " Sah ra " # name mit Leerzeichen links sowie rechts
 
@@ -21,13 +21,13 @@ print(name_with_spaces.strip())
 # only temporary stripes both left and right' Sah rah '
 
 # print(name_mit_space.rstrip())
-#stripes spaces of the right site (also temporary)
+# stripes spaces of the right site (also temporary)
 
 # print(name_mit_space.lstrip()) 
-#stripes spaces of the left site (also temporary)
+# stripes spaces of the left site (also temporary)
 
 # if you want to strip the value of a variable permanently you have 
-#to change the value manually
+# to change the value manually
 name_with_spaces = name_with_spaces.strip()
 print(name_with_spaces) # now with out spaces
 
