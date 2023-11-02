a30f649c10af14a681314714b6f1d7ee967646c0
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-11-03 00:04:22 +0100

    Delete voting.py

--- voting.py
+++
@@ -1,9 +0,0 @@
-age1 = 19
-age2 = 17
-
-if age2 >= 18:
-    print("You are old enough to vote!")
-    print("Have you registered to vote yet?")
-else:
-    print("Sorry, you are too young to vote.")
-    print("Please register to vote as soon as you turn 18!")
