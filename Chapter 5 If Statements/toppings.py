c9e7405f1bcedbcef07cd704116b98e7c5cb53d3
Bennet <79263067+NerdyBennet@users.noreply.github.com>
2023-11-03 00:05:15 +0100

    Add files via upload

--- Chapter 5 If Statements/toppings.py
+++ Chapter 5 If Statements/toppings.py
@@ -2,7 +2,10 @@ requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
 for requested_topping in requested_toppings: 
     # requested_topping without
-    #the s
-    print(f"Adding {requested_toppings}.")
+    # the s
+    if requested_topping == 'green peppers':
+        print('Sorry, we are out of green peppers right now.')
+    else:
+        print(f"Adding {requested_topping}.")
 
 print("\nFinished making your pizza!")
